d7f485b9b876bb000e25bd3dfd81d16a2fc9e212
Garima Tomar <garimatomar55521@gmail.com>
2025-05-08 22:29:48 +0530

    stack & Heap memory learning

--- style.js
+++ style.js
@@ -1,1 +1,20 @@
 
+let myname = "Garima Tomar";
+
+let secondname = "Golu";
+secondname = "kattu";
+
+
+ console.log(secondname);
+ console.log(myname);
+
+
+ let userone = {
+    email: "garima@google.com",
+    password:"Garima@123",
+    upi: "garima@upi",
+    name: "Garima",
+    age: 26,
+    isLoggedIn: false,
+    lastLoginDays: 30,      
+ }
