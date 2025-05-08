0c8fefcd1de5d8ae6f041ea48e8f1b52c1d8a4c4
Garima Tomar <garimatomar55521@gmail.com>
2025-05-08 22:43:57 +0530

    heap memory learning

---
+++ Learning Javascript Topics/heap&stackmemory.js
@@ -0,0 +1,29 @@
+
+let myname = "Garima Tomar";
+
+let secondname = "Golu";
+secondname = "kattu";
+
+
+ console.log(secondname);
+ console.log(myname);
+
+
+ let userone = {
+    email: "garima@google.com",
+    password:"Garima@123",
+    upi: "garima@upi",
+    name: "Garima",
+    age: 26,
+    isLoggedIn: false,
+    lastLoginDays: 30,      
+ }
+
+
+
+ let usertwo = userone;
+
+ usertwo.email = "garimatomar@google.com";
+
+ console.log(userone.email);
+ console.log(usertwo.email);
