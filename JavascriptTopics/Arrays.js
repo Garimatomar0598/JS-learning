c1bd60042d4750a610d4aa1104d5f766a52c7dbb
Garima Tomar <garimatomar55521@gmail.com>
2025-05-23 13:43:23 +0530

    End of arrays

--- JavascriptTopics/Arrays.js
+++ JavascriptTopics/Arrays.js
@@ -6,3 +6,15 @@ const dc_heros = ["batman", "superman", "batman"];
 // console.log(marvel_heros[3][1]);
 
 
+const new_heros =[...marvel_heros,...dc_heros];
+//console.log(new_heros);
+
+
+const another_array = [1,2,3,[4,5,6],[7,8,9],[10,11]];
+
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
+
+
+
+
