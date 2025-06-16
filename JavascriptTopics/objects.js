786d1f4fc13eb8000c1d796821adde172d342076
Garima Tomar <garimatomar55521@gmail.com>
2025-06-16 09:26:51 +0530

    difference btween textContend & nnerHtml learning

--- JavascriptTopics/objects.js
+++ JavascriptTopics/objects.js
@@ -1,3 +1,19 @@
+//what is object
+//object ek tarika hai jisse ki hum  ek identity ki details lo ek sath rakh sakte hai
+
+var obj = {};
+var obj5 = new Object();
+
+
+
+
+
+
+
+
+
+
+
 //object literal
 
 const mysym= Symbol("key1");
@@ -22,14 +38,14 @@ const JsUser = {
 
 
 JsUser.greeting = function(){
-    console.log('Hello Js user');
+    //console.log('Hello Js user');
 
 }
 
 
 JsUser.greetingTwo = function(){
 
-console.log('Hello Js user 2 ,${this.age}');
+//console.log('Hello Js user 2 ,${this.age}');
     
 }
 
@@ -38,9 +54,43 @@ console.log('Hello Js user 2 ,${this.age}');
 //console.log(JsUser.greetingTwo());
 //---------------------------------//
 
+// singleton object
+
+//const tinderUser = new Object(); // singleton
+
+const tinderUser = {}    // non-singleton object
+tinderUser.id = "123abc";
+tinderUser.name = "Garima";
+tinderUser.age = 26;
+tinderUser.isloggedIn = false;
+
+//console.log(tinderUser);
+
+
+const regularUser = {
+    email: "someone@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname:"Garima",
+            lastname:"Tomar"
+        }
+    }
+}
+
+
+//console.log(regularUser.fullname?.userfullname.firstname);
 
 
 
+
+const obj1 = {1:"a", 2:"b", 3:"c"};
+const obj2 = {4:"d", 5:"e", 6:"f"};
+
+
+//const obj3 = {obj1,obj2};
+
+   const obj3 = Object.assign({},obj1,obj2)
+//console.log(obj3);
 
 
 
