1c8ce4ec7d352ed92238f0c79ca16d11aa3e5ecd
Garima Tomar <garimatomar55521@gmail.com>
2025-06-17 18:31:37 +0530

    question solving

--- JavascriptTopics/script.js
+++ JavascriptTopics/script.js
@@ -1,8 +1,15 @@
 
-var  btn = document.querySelector("button");
-var p = document.querySelector("p");
+
+var img1 = document.querySelector("#one");
+var img2 = document.querySelector("#two");
+
+document.querySelector("button")
+.addEventListener("click",function(){
+ var src1 = img1.src;
+ var src2= img2.src;
+
+img1.src = src2;
+img2.src = src1;
 
 
-btn. addEventListener("click",function(){
- console.log("change the stuff")
 })
