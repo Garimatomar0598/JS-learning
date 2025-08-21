508cbeb102d36a96bf3ddbe6942cd97068113988
Garima Tomar <garimatomar55521@gmail.com>
2025-08-21 14:00:33 +0530

    this keyword understanding

--- JavascriptTopics/map.js
+++ JavascriptTopics/map.js
@@ -1,7 +1,6 @@
 const values = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-const newvalues = values.filter((num)=>{
-  return num > 8
-})
+const newvalues = values.filter((num)=> num > 8
+)
 
 console.log(newvalues);
 
