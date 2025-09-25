f877961c654eb4cfe65e634931896d329dcfbfbb
Garima Tomar <garimatomar55521@gmail.com>
2025-09-25 09:59:18 +0530

    Different consitions main this ki value aalag aalga hoti ha in js

--- JavascriptTopics/thiskeyword.js
+++ JavascriptTopics/thiskeyword.js
@@ -1,1 +1,8 @@
 console.log(this);
+//this ki value global main- window
+//method main this ki value-object
+//function main this ki value- window
+//function inside method (Es6)- object
+//function inside method in es5 -window
+//constructor fnc main this ki value- new blank object
+//event listner main this ki value-  that element jis pr event listner laga ho
